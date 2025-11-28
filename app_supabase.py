e9b464de1d71c194352a7df80a187e0b4cd1889c
candicecooper <candice.cooper330@schools.sa.edu.au>
2025-11-28 14:02:55 +1030

    Update app_supabase.py

--- app_supabase.py
+++ app_supabase.py
@@ -500,6 +500,7 @@ def show_severity_guide():
     </div>
 </div>
 """, unsafe_allow_html=True)
+    
 def send_critical_incident_email(incident_data, student, staff_email, leader_email, admin_email):
     """Send email notification to all parties"""
     st.info(f"""📧 **Email Notification Sent**
